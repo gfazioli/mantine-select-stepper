c8b5fdb6ad6aecf717488aea3018ce2621776f17
Giovambattista Fazioli <gfazioli@users.noreply.github.com>
2026-01-14 17:54:44 +0100

    Initial commit

---
+++ docs/components/PropsTable/getComponentName.ts
@@ -0,0 +1,8 @@
+interface GetComponentNameInput {
+  component: string;
+  componentPrefix: string | undefined;
+}
+
+export function getComponentName({ component, componentPrefix }: GetComponentNameInput) {
+  return componentPrefix ? (componentPrefix === component ? component : `${componentPrefix}.${component.replace(componentPrefix, '')}`) : component;
+}
