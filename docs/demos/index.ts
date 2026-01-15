d89d44f8e7a7529fe1116e1d8ab58502f4bf3293
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-01-15 16:53:58 +0100

    ♻️ refactor: rename Led demo files to SelectStepper and drop defcon demo
    - Align demo filenames with component name
    - Remove unused defcon demo and its export
    - Update demos index to reflect new filenames

--- docs/demos/index.ts
+++ docs/demos/index.ts
@@ -2,7 +2,6 @@ export { animations } from './SelectStepper.demo.animations';
 export { colors } from './SelectStepper.demo.colors';
 export { configurator } from './SelectStepper.demo.configurator';
 export { controlled } from './SelectStepper.demo.controlled';
-export { defcon } from './SelectStepper.demo.defcon';
 export { labels } from './SelectStepper.demo.labels';
 export { statusPanel } from './SelectStepper.demo.statusPanel';
 export { stylesApi } from './SelectStepper.demo.stylesApi';
